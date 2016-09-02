9e8d5d10482d55abe5e125f2ac15107d26fa5424
B1nj0y <v@joyv.net>
2016-09-02 18:59:56 +0800

    format the rover.hs

--- Haskell/rover.hs
+++ Haskell/rover.hs
@@ -25,20 +25,23 @@ getStartPos str = ((toSteps x, toSteps y), d)
 
 nextDirect :: Direction -> Movement -> Direction
 nextDirect d "M" = d
-nextDirect d "L" = case d of "W" -> "S"
-                             "S" -> "E"
-                             "E" -> "N"
-                             "N" -> "W"
-nextDirect d "R" = case d of "W" -> "N"
-                             "N" -> "E"
-                             "E" -> "S"
-                             "S" -> "W"
+nextDirect d "L" = case d of
+                       "W" -> "S"
+                       "S" -> "E"
+                       "E" -> "N"
+                       "N" -> "W"
+nextDirect d "R" = case d of
+                       "W" -> "N"
+                       "N" -> "E"
+                       "E" -> "S"
+                       "S" -> "W"
 
 nextCoordinate :: Coordinate -> Direction -> Movement -> Coordinate
-nextCoordinate (x, y) d "M" = case d of "W" -> (x - 1, y)
-                                        "E" -> (x + 1, y)
-                                        "N" -> (x, y + 1)
-                                        "S" -> (x, y - 1)
+nextCoordinate (x, y) d "M" = case d of
+                                  "W" -> (x - 1, y)
+                                  "E" -> (x + 1, y)
+                                  "N" -> (x, y + 1)
+                                  "S" -> (x, y - 1)
 nextCoordinate (x, y) _ _ = (x, y)
 
 move :: MoveInfo -> Maybe MoveInfo
@@ -49,8 +52,9 @@ move (((x, y), d), ms) = Just ((nc, nd), tail ms)
 
 -- check if the rover in the valid area after a series of runs
 checkResult :: Coordinate -> Maybe MoveInfo -> String
-checkResult mc (Just (((x, y), d), _)) | (x, y) `elem` allPos = show x ++ " " ++ show y ++ " " ++ d
-                                       | otherwise = "RIP"
+checkResult mc (Just (((x, y), d), _))
+    | (x, y) `elem` allPos = show x ++ " " ++ show y ++ " " ++ d
+    | otherwise            = "RIP"
   where allPos = [ (x, y) | x <- [0..fst mc], y <- [0..snd mc] ]
 
 runInstructs :: [String] -> (Int, Int) -> Maybe MoveInfo
@@ -60,6 +64,14 @@ runInstructs xs (m, n) = Just (sp, map (:[]) ins) >>= foldr (<=<) return (replic
 
 main :: IO ()
 main = do
-    let instructs = ["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM", "4 4 N", "MMM"]
+    let instructs = [ "5 5"
+                    , "1 2 N"
+                    , "LMLMLMLMM"
+                    , "3 3 E"
+                    , "MMRMMRMRRM"
+                    , "4 4 N"
+                    , "MMM"
+                    ]
         mc = getMaxCoordinate $ head instructs
-    mapM_ print . map (checkResult mc . runInstructs instructs) $ take (length instructs `div` 2) $ zip [1,3..] [2,4..]
+    mapM_ print . map (checkResult mc . runInstructs instructs)
+                      $ take (length instructs `div` 2) $ zip [1,3..] [2,4..]
